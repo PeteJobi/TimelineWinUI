093a6f34b01f2b22f79809c2a4bbc4fe33d17543
Peter Egunjobi <masterpdon@gmail.com>
2025-10-10 02:16:28 +0100

    Clean up using statements

--- MediaTimeline.cs
+++ MediaTimeline.cs
@@ -1,17 +1,14 @@
 ﻿using Microsoft.UI.Dispatching;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Imaging;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Timeline;
 using Windows.Media.Playback;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Media.Imaging;
 
 namespace Timeline
 {
