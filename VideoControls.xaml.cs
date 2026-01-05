ff7814c051cc7c3271da807428899438226e72c4
Peter Egunjobi <masterpdon@gmail.com>
2026-01-05 21:19:13 +0100

    TimeSpanTextBox now exists in a different library. Fix references.

--- VideoControls.xaml.cs
+++ VideoControls.xaml.cs
@@ -18,6 +18,7 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Media.Playback;
 using Microsoft.UI.Dispatching;
+using TextToTimeSpan;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
